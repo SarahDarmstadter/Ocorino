adaac9c4d4c18dd0e395f6bd7697bc1b69e1ba18
Sarah Darmstadter <sarahdarmstadter@gmail.com>
2021-06-09 17:28:05 +0200

    page index et panier

--- Script_accueil.js
+++ Script_accueil.js
@@ -1,11 +1,16 @@
 
-/* Template encart produit*/
+/*Template encart produit */
 
-let cardsProduit = document.getElementsByClassName("cards_produit");
+/* let cardsProduit = document.getElementsByClassName("cards_produit");
+ 
+cardsProduit.appenChild(newCard);
+
+
+
 
 let newCard = document.createElement("div");
     newCard.classList.add("cardJS");
-cardsProduit.appendChild(newCard);
+
 
 let title = document.createElement("h2");
     title.classList.add("nom_produit");
@@ -21,7 +26,97 @@ let imgProduit = document.createElement("img");
     imgProduit.classList.add("img_produit");
     imgProduit.src = "file:///C:/Users/sarah/OneDrive/Desktop/Ocarino/logo_ocarino.PNG"
 newCard.appendChild(imgProduit);
+*/
 
+
+
+
+
+
+
+/*CREATION FONCTION MAIN POUR STOCKER LES ARTICLES???
+   async function articleEnVente () {
+        const data = await getData();
+        displayData(data);
+    }
+*/
+
+/*CREATION FONCTION POUR RECUPERER LES DONNEES*/
+
+fetch('http://localhost:3000/api/cameras') 
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        data.forEach(function(produit){
+            console.log('produit', produit)
+            createCard(produit)
+        })
+    })
+    .catch(function(error) {
+        console.error(error);
+    });
+
+
+    let cardsProduit = document.getElementById("cards_produit");
+    console.log(cardsProduit);
+
+function createCard(data) {
+
+
+// card sert à créer la div. 
+    let card = document.createElement("div");
+    card.classList.add("newCard");
+
+// Récuperer le parent qui contient les div que l'on a créées
+    let cardsProduit = document.getElementById("cards_produit");
+// On met la div dans le DOM. 
+    cardsProduit.appendChild(card);
+
+
+// newCard sert à aller chercher LES div avec comme classe newCard vu que mnt elles sont dans le DOM.
+    let newCard = document.getElementsByClassName("newCard");
+        
+    let lien = document.createElement("a");
+        lien.classList.add("lien");
+        lien.href = "produit.html?id=" + data._id;
+
+    let id = document.createElement("p");
+        id.classList.add("id");
+        id.textContent = data._id;
+        newCard[newCard.length - 1].appendChild(id);
+
+    let name = document.createElement("p");
+        name.classList.add("name");
+        name.textContent = data.name;
+        newCard[newCard.length - 1].appendChild(name);
+    
+    let description = document.createElement("p");
+        description.classList.add("description");
+        description.textContent = data.description; 
+        newCard[newCard.length - 1].appendChild(description)
+    
+    let price = document.createElement("p");
+        price.classList.add("price");
+        price.textContent += data.price + " euros"; 
+        newCard[newCard.length - 1].appendChild(price)
+    
+    let img = document.createElement("img");
+        img.classList.add("img_produit");
+        img.src = data.imageUrl ; 
+        newCard[newCard.length - 1].appendChild(img)
+    
+    lien.appendChild(newCard[newCard.length - 1])
+    cardsProduit.appendChild(lien)
+}
+
+
+//ajouter le lien comme price et copagnie avec data._id en 
+
+   
+    
+
+    
 
 
     
---
+++ produit.js
@@ -0,0 +1,94 @@
+// Creer une fonction qui va lire l'id dans l'url de la page produit. 
+// passer l'id en parametre de fetch pour recup les infos de ce produit. 
+// display les infos sur la page. 
+    
+
+
+// creer un bouton de personnalisation et stocker le choix du client. 
+// ajouter au panier : 
+    // envoyer l'info au serveur POST 
+    // quand on ajoute au panier ya un 1 qui apparait en haut de la navigation coté panier. 
+    // permettre de choisir la quantité à mettre au panier 
+
+
+/*CREATION D'UNE FONCTION POUR LIRE L'URL et récupérer l'id */
+//le programme doit lire l'url 
+// récupérer l'id 
+// retourner l'id
+
+function getIdFromUrl(){
+    let url = window.location.toString();
+    let splitUrl = url.split("\=");
+    let id = splitUrl[1];
+ return id 
+}
+
+/*2. FETCH DES INFORMATIONS DU PRODUIT VIA L'ID*/
+
+function getProductInfo() {
+// En appelant getIdFromUrl dans la fonction, on evite de l'appeler pour rien en dehors et d'encombrer la stack pour rien. 
+    let id = getIdFromUrl();
+// Ajout de l'id dans l'url pour qu'il sache où aller et le / car c'est dans les spec. 
+    let url = "http://localhost:3000/api/cameras/" + id; 
+    fetch(url) 
+        .then (function(response) {
+            return response.json()
+        })
+        .then(function(data) {
+            fonction1(data);
+            fonction2(data);
+            console.log(data)
+        })
+        .catch(function(error) {
+            console.error(error);
+        });      
+}
+
+/* Créer DES fonctions qui afficheront les infos produit sur la page HTML 
+et accessoirement créer le HTML*/
+//fonction 1 
+    //creation div image 
+    // creation info "texte"
+            //prix nom description 
+//fonction 2
+    // bouton de personnalisation 
+        //
+        //
+// fonction 3 
+    // ajout au panier 
+        // 
+        //
+
+
+
+// Puisqu'on a appele fonction1 avec le parametre data, JS comprend que fonction1 a un parametre 
+// et ce peu importe son nom. Data ou produit. si c'est la meme fonction c'est le meme parametre        
+
+
+function fonction1(produit) {
+    let imageBox = document.getElementById("produit__image");
+    let imgProduit = document.createElement("img");
+        imgProduit.src = produit.imageUrl;
+    imageBox.appendChild(imgProduit);
+
+// A TERMINER 
+}
+
+function fonction2(produit) {
+    let dropdownList = document.getElementById("produit__personnalisation");
+    produit.lenses.forEach(function(choix) {
+        let option = document.createElement("option");
+        option.value = choix;
+        option.textContent = choix;
+        dropdownList.appendChild(option);
+    })
+
+
+
+
+
+
+}
+
+
+getProductInfo();
